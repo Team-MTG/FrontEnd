57335c8fec6a0f66f3dd9cabc02078b6f99fa391
chb09876 <chb09876@naver.com>
2023-01-07 19:40:42 +0900

    feat: 한국 숫자 단위 표시 함수 추가

--- src/utils/prettyKorNum.js
+++ src/utils/prettyKorNum.js
@@ -1,17 +1,25 @@
 const prettyKorNum = (num) => {
   const units = ['', '만', '억', '조', '경'];
-  return units.reduce((prev, unit, index) => {
-    if (num > 0) {
-      if (index > 1) {
-        prev = prev.substring(0, prev.lastIndexOf(units[index - 1])) + units[index - 1];
+  let minus = '';
+  if (num < 0) {
+    minus = '-';
+    num = -num;
+  }
+  return (
+    minus +
+    units.reduce((prev, unit, index) => {
+      if (num > 0) {
+        if (index > 1) {
+          prev = prev.substring(0, prev.lastIndexOf(units[index - 1])) + units[index - 1];
+        }
+        const split = num % 10000;
+        num = Math.floor(num / 10000);
+        if (split === 0) return prev;
+        return split.toLocaleString('ko-kr') + unit + prev;
       }
-      const split = num % 10000;
-      num = Math.floor(num / 10000);
-      if (split === 0) return prev;
-      return split.toLocaleString('ko-kr') + unit + prev;
-    }
-    return prev;
-  }, '');
+      return prev;
+    }, '')
+  );
 };
 
 export default prettyKorNum;
