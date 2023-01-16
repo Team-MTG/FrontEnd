9645d8e386f16b5d99fa639f193e1a0b62061019
mouse0429 <mouse0429@naver.com>
2023-01-17 01:19:55 +0900

    [화면]최종 결과 화면 데이터 연결
    - 이름 글자 수에 따른 예외 처리
    - 데이터 연결

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,10 +1,20 @@
 import { Navigate, useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import prettyKorNum from './utils/prettyKorNum';
 import rankBtn from './assets/rankBtn.svg';
+import { SEED_MONEY } from './config';
+import { userCashState, userNameState, userRateState } from './atoms/user';
 
 export default function Result() {
   const navigate = useNavigate();
 
+  /*데이터*/
+  const userName = useRecoilValue(userNameState);
+  const totalProfit = useRecoilValue(userRateState) * 100;
+  const totalBalance = useRecoilValue(userCashState);
+  const totalYield = totalBalance - SEED_MONEY;
+
+  /*상세 결과 컴포넌트화*/
   function detailResult(title, data, unit, decoration = false, fontSize = 'xs') {
     return (
       <div className="flex flex-col items-center">
@@ -23,15 +33,21 @@ export default function Result() {
 
   return (
     <div className="bg-[url('../src/assets/bg-resultpage.svg')] bg-contain bg-no-repeat bg-center h-[38rem] w-96 mx-auto flex flex-col justify-between items-center mt-[8vh]">
-      <span className="relative top-28 font-sc text-base leading-none font-bold">
-        <span className="font-tn text-3xl">조하빈</span>님의 투자 결과
+      <span
+        className={`relative top-${
+          userName.length > 8 ? 24 : 28
+        } font-sc text-base leading-none font-bold text-center`}
+      >
+        <span className="font-tn text-3xl">{userName}</span>
+        {userName.length > 8 ? <br /> : <></>}
+        님의 투자 결과
       </span>
 
       <div className="h-[21rem] py-[20px] relative top-12 font-sc font-bold flex flex-col justify-between items-center">
-        {detailResult('시드머니', 500000, '원', false, 'base')}
-        {detailResult('평가손익', 1571566, '원')}
-        {detailResult('잔고평가', 1231235, '원')}
-        {detailResult('총 수익률', 122.94, '%', true)}
+        {detailResult('시드머니', SEED_MONEY, '원', false, 'base')}
+        {detailResult('평가손익', totalYield, '원')}
+        {detailResult('잔고평가', totalBalance, '원')}
+        {detailResult('총 수익률', totalProfit, '%', true)}
       </div>
 
       <button
