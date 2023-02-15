b70ae9b229af984368a83c85b718d4dcc5508fd0
chb09876 <chb09876@naver.com>
2023-02-15 19:50:25 +0900

    [기타] 메인페이지 노션 링크 추가

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ export const MAX_SEC = 15;
 export const MAX_PHASE = 5;
 export const MAX_STOCKS_INDEX = 27;
 export const URL_GITHUB = 'https://github.com/Team-MTG';
-export const URL_NOTION = 'https://www.notion.so/e2de89087a894ccbbd58b8a395ba1355';
+export const URL_NOTION =
+  'https://battle-cupboard-4cc.notion.site/d228cae50ccf438ab5b9413e320ab86d';
 
 export const DEBUG = false;
 export const DEBUG_STOCKS_INDEX_LIST = [1, 2, 3];
